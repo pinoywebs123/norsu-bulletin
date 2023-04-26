ccae5c9132219a606787aff24a07313979205df4
Morls <moravel752@gmail.com>
2023-04-26 12:08:03 +0800

    group middleware auth

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,6 @@ use App\Http\Controllers\AuthController;
 
 
 
-Route::get('/users', [UsersController::class, 'users']);
-Route::post('/user', [UsersController::class, 'users_check'])->name('users_check');
-Route::post('/delete-user', [UsersController::class, 'users_delete'])->name('users_delete');
 
 
 Route::get('/', [AuthController::class, 'home'])->name('home');
@@ -22,16 +19,27 @@ Route::get('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/login-check', [AuthController::class, 'login_check'])->name('login_check');
 
 
-Route::get('/category', [CategoryController::class, 'category']);
-Route::get('/new-category', [CategoryController::class, 'new_category']);
-Route::post('/check-category', [CategoryController::class, 'check_category'])->name('category_check');
-Route::post('/delete-category', [CategoryController::class, 'category_delete'])->name('category_delete');
-Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
 
-Route::get('/bulletin', [AdminController::class, 'bulletin'])->name('bulletin');
-
-Route::post('/bulletin', [AdminController::class, 'bulletin_check'])->name('bulletin_check');
-Route::post('/delete-bulletin', [AdminController::class, 'bulletin_delete'])->name('bulletin_delete');
 
 Route::get('/news', [Controller::class, 'news'])->name('news');
 Route::get('/news/{id}', [Controller::class, 'show_news'])->name('show_news');
+
+Route::group(['middleware'=> 'auth'], function(){
+
+
+    Route::get('/users', [UsersController::class, 'users']);
+    Route::post('/user', [UsersController::class, 'users_check'])->name('users_check');
+    Route::post('/delete-user', [UsersController::class, 'users_delete'])->name('users_delete');
+
+    Route::get('/category', [CategoryController::class, 'category']);
+    Route::get('/new-category', [CategoryController::class, 'new_category']);
+    Route::post('/check-category', [CategoryController::class, 'check_category'])->name('category_check');
+    Route::post('/delete-category', [CategoryController::class, 'category_delete'])->name('category_delete');
+    Route::get('/logout', [AdminController::class, 'logout'])->name('logout');
+
+    Route::get('/bulletin', [AdminController::class, 'bulletin'])->name('bulletin');
+
+    Route::post('/bulletin', [AdminController::class, 'bulletin_check'])->name('bulletin_check');
+    Route::post('/delete-bulletin', [AdminController::class, 'bulletin_delete'])->name('bulletin_delete');
+
+});
