be24e4138923f6da835219f38917736f89cb7a35
Morls <moravel752@gmail.com>
2023-04-27 09:16:56 +0800

    bulletin jquery

--- resources/views/admin/bulletin.blade.php
+++ resources/views/admin/bulletin.blade.php
@@ -348,7 +348,7 @@
     </div>
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{URL::to('vendor/jquery/jquery.min.js')}}"></script>
+    <!-- <script src="{{URL::to('vendor/jquery/jquery.min.js')}}"></script> -->
     <script src="{{URL::to('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 
     <!-- Core plugin JavaScript-->
